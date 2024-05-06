bb0f2625f2ba15b3f33e8eb35031f16ebef5477d
Mueza Ejaz <159851369+Mueza-Ejaz@users.noreply.github.com>
2024-05-07 04:09:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const Currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: chalk.green("\n \t Enter From Currency"),
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        type: "list",
+        message: chalk.blue("\n \t Enter To Currency"),
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: chalk.blue("\n \tEnter Your Amount..."),
+    }
+]);
+let fromAmount = Currency[user_answer.from];
+let toAmount = Currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
